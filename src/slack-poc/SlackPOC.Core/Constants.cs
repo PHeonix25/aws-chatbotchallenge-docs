d11b26ed7082cef1676e3e0844d4c38aa6f0ab52
PHeonix25 <p@hermens.com.au>
2017-06-17 00:40:54 +0200

    Adds additional constants

--- src/slack-poc/SlackPOC.Core/Constants.cs
+++ src/slack-poc/SlackPOC.Core/Constants.cs
@@ -1,7 +1,11 @@
 ﻿namespace SlackPOC.Core
 {
-    public class Constants
+    public static class Constants
     {
-        public const string DEFAULT_WELCOME = "Hello World!";
+        public const string CLIENT_ID       = "";
+        public const string CLIENT_SECRET   = "";
+        public const string CALLBACK_URI    = ""; // Don't forget to update this here: https://api.slack.com/apps/A5W02V286/oauth
+        public const string CHANNEL_NAME    = "";
+        public const string TEAM_NAME       = "";
     }
 }
