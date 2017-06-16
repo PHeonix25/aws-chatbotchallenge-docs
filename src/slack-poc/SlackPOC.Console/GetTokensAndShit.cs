8ac22c78ccad1ade38d02c53e9523d011ce34050
PHeonix25 <p@hermens.com.au>
2017-06-17 00:42:50 +0200

    First attempt at posting something

---
+++ src/slack-poc/SlackPOC.Console/GetTokensAndShit.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.AspNetCore.WebUtilities;
+using SlackAPI;
+using SlackPOC.Core;
+
+namespace SlackPOC
+{
+    public class GetTokensAndShit
+    {
+        public static OAuthResponse CompleteOAuth()
+        {
+            var state = Guid.NewGuid().ToString();
+            
+            var uri = SlackClient.GetAuthorizeUri(Constants.CLIENT_ID, 
+                SlackScope.Identify | SlackScope.Read | SlackScope.Post, 
+                Constants.CALLBACK_URI, 
+                state, 
+                Constants.TEAM_NAME);
+
+            Console.WriteLine("Launching: " + uri);
+
+            OpenBrowserHack.OpenBrowser(uri.ToString());
+
+            // Come back here and make the HTTP response land in our console app - it's .Net Core after all :)
+            Console.WriteLine("Please paste in the URL of the authentication result...");
+            var asString = Console.ReadLine();
+            var index = asString.IndexOf('?');
+            if (index != -1)
+                asString = asString.Substring(index + 1);
+
+            var qs = QueryHelpers.ParseQuery(asString);
+            var code = qs["code"];
+            var newState = qs["state"];
+
+            // validate the state. this isn't required, but let's makes sure the request and response line up...
+            if (state != newState)
+                throw new InvalidOperationException("State mismatch. You're trying to trick me!");
+
+            return new OAuthResponse(code, newState);
+        }
+    }
+}
---
+++ src/slack-poc/SlackPOC.Console/OutgoingMessages.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using SlackAPI;
+using SlackPOC.Core;
+
+namespace SlackPOC
+{
+    public class OutgoingMessages
+    {
+        public static void PostMessage(OAuthResponse oAuthResponse, string message)
+        {
+            try
+            {
+                // then get the token...
+                Console.WriteLine("Requesting access token...");
+                SlackClient.GetAccessToken((response) =>
+                {
+                    var accessToken = response.access_token;
+                    if (string.IsNullOrWhiteSpace(accessToken))
+                        Console.WriteLine("AccessToken was not provided. Error reason: " + response.error);
+                    else
+                    {
+                        Console.WriteLine("Got access token '{0}'...", accessToken);
+
+                        //post...
+                        var client = new SlackClient(accessToken);
+                        client.PostMessage((r) => Console.WriteLine(r), Constants.CHANNEL_NAME, message);
+                    }
+
+                    // finished...
+                    Console.WriteLine("Done.");
+
+                }, Constants.CLIENT_ID, Constants.CLIENT_SECRET, Constants.CALLBACK_URI, oAuthResponse.OAuthToken);
+
+                Console.WriteLine("Awaiting results of the AccessToken callback");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
